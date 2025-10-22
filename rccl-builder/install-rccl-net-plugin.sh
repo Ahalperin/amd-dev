9f00252839a29603134cf0dd496ef2ddf6d923ce
Amir Halperin <ahalperin@drivenets.com>
2025-10-22 13:00:47 -0500

    add install-rccl-net-plugin script

---
+++ rccl-builder/install-rccl-net-plugin.sh
@@ -0,0 +1,9 @@
+cd ~/amd-dev/amd
+git clone https://github.com/rocm/amd-anp
+cd amd-anp
+git checkout tags/v1.1.0-5
+git switch -c v1.1.0-5
+
+# build and install rccl-network plugin (depends on AINIC driver that is installed on bare-metal)
+sudo make RCCL_HOME=/home/dn/amd-dev/amd/rccl/ MPI_INCLUDE=/opt/ompi-4.1.6/build/ompi/include/ MPI_LIB_PATH=/opt/ompi-4.1.6/build/ompi/.libs/ ROCM_PATH=/opt/rocm-7.0.1/
+sudo make RCCL_HOME=/home/dn/amd-dev/amd/rccl/ ROCM_PATH=/opt/rocm-7.0.1/ install
