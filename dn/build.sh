1be731d774fa16b74b0a6efb5823cf0d8b47cf4c
Amir Halperin <ahalperin@drivenets.com>
2025-11-07 00:20:02 -0600

    install amd-anp in rocm after build

--- dn/build.sh
+++ dn/build.sh
@@ -35,4 +35,4 @@ cd /home/dn/amd-dev/dn/rccl && sudo rm -rf build && ./install.sh -l --prefix bui
 cd /home/dn/amd-dev/dn/rccl-tests/ && sudo rm -rf build && make MPI=1 MPI_HOME=${OMPI_HOME} NCCL_HOME=${RCCL_INSTALL_DIR} -j
 # build and install rccl-network plugin (depends on AINIC driver that is installed on bare-metal)
 cd /home/dn/amd-dev/dn/amd-anp && sudo rm -rf build && sudo make RCCL_HOME=${RCCL_HOME} MPI_INCLUDE=${OMPI_HOME}/include/ MPI_LIB_PATH=${OMPI_HOME}/lib ROCM_PATH=${ROCM_HOME}
-# cd /home/dn/amd-dev/dn/amd-anp && sudo make RCCL_HOME=${RCCL_HOME} ROCM_PATH=${ROCM_HOME} install
+cd /home/dn/amd-dev/dn/amd-anp && sudo make RCCL_HOME=${RCCL_HOME} ROCM_PATH=${ROCM_HOME} install
