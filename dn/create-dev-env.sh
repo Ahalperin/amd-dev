6b8d804dfa9ee2731316621c22638ae9659202d4
Amir Halperin <ahalperin@drivenets.com>
2025-11-26 02:19:15 -0600

    update create-dev-env.sh to fetch also meta/torchcomms repo

--- dn/create-dev-env.sh
+++ dn/create-dev-env.sh
@@ -8,19 +8,42 @@ DN_DIR=$(dirname "$0")
 
 # clone amd repos & checkout to specific branches
 pushd ${DN_DIR}
-git clone --recurse-submodules -b develop git@github.com:Ahalperin/rccl.git
-cd rccl
-git remote add upstream https://github.com/ROCm/rccl.git
+if [ ! -d "rccl" ]; then
+  git clone --recurse-submodules -b develop git@github.com:Ahalperin/rccl.git
+  cd rccl
+  git remote add upstream https://github.com/ROCm/rccl.git
+else
+  echo "Skipping rccl - repository already exists"
+fi
 popd
 
 pushd ${DN_DIR}
-git clone --recurse-submodules -b develop git@github.com:Ahalperin/rccl-tests.git
-cd rccl-tests
-git remote add upstream https://github.com/ROCm/rccl-tests.git
+if [ ! -d "rccl-tests" ]; then
+  git clone --recurse-submodules -b develop git@github.com:Ahalperin/rccl-tests.git
+  cd rccl-tests
+  git remote add upstream https://github.com/ROCm/rccl-tests.git
+else
+  echo "Skipping rccl-tests - repository already exists"
+fi
 popd
 
 pushd ${DN_DIR}
-git clone git@github.com:Ahalperin/amd-anp.git
-cd amd-anp
-git remote add upstream https://github.com/rocm/amd-anp.git
+if [ ! -d "amd-anp" ]; then
+  git clone git@github.com:Ahalperin/amd-anp.git
+  cd amd-anp
+  git remote add upstream https://github.com/rocm/amd-anp.git
+else
+  echo "Skipping amd-anp - repository already exists"
+fi
+popd
+
+# clone meta repos & checkout to specific branches
+pushd ${DN_DIR}
+if [ ! -d "torchcomms" ]; then
+  git clone git@github.com:Ahalperin/torchcomms.git
+  cd torchcomms
+  git remote add upstream https://github.com/meta-pytorch/torchcomms.git
+else
+  echo "Skipping torchcomms - repository already exists"
+fi
 popd
